e372f9121f1124fab1273d8bdd7b72bd7a16f93b
JaichandranR <highmast@gmail.com>
2025-05-07 14:45:53 -0400

    Update createtable.py

--- createtable.py
+++ createtable.py
@@ -1,7 +1,4 @@
-def test_sync_table_partitions(self):
-        source_db = 'src'
-        target_db = 'tgt'
-        table_name = 'test_table'
+        glue = mock.MagicMock()
 
         mock_partition = {
             'Values': ['2024'],
@@ -9,25 +6,19 @@ def test_sync_table_partitions(self):
             'Parameters': {}
         }
 
-        glue = mock.MagicMock()
+        glue.batch_create_partition = mock.MagicMock()
 
-        # Create separate paginator mocks for source and target
-        src_paginator = mock.MagicMock()
-        tgt_paginator = mock.MagicMock()
-
-        src_paginator.paginate.return_value = [{'Partitions': [mock_partition]}]
-        tgt_paginator.paginate.return_value = [{'Partitions': []}]
-
-        # Setup side effect so it returns different paginator based on call
         def get_paginator_side_effect(op):
-            return src_paginator if op == 'get_partitions' else tgt_paginator
+            paginator = mock.MagicMock()
+            if op == 'get_partitions':
+                paginator.paginate.side_effect = lambda **kwargs: (
+                    [{'Partitions': [mock_partition]}] if kwargs['DatabaseName'] == source_db
+                    else [{'Partitions': []}]
+                )
+            return paginator
 
         glue.get_paginator.side_effect = get_paginator_side_effect
 
-        glue.batch_create_partition = mock.MagicMock()
-
-        # Call the function
         common_data_sync.sync_table_partitions(source_db, target_db, table_name, glue)
 
-        # Assert batch_create_partition is called
         glue.batch_create_partition.assert_called_once()
