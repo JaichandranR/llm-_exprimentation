ead9bb43a2cfa294d4af22e8b026c65b8919941e
JaichandranR <highmast@gmail.com>
2025-05-07 14:36:51 -0400

    Update createtable.py

--- createtable.py
+++ createtable.py
@@ -1,35 +1,29 @@
-...
-    def test_sync_table_partitions(self):
-        source_db = 'src'
-        target_db = 'tgt'
-        table_name = 'test_table'
+def test_sync_table_partitions(self):
+    source_db = 'src'
+    target_db = 'tgt'
+    table_name = 'test_table'
 
-        mock_partition = {
-            'Values': ['2024'],
-            'StorageDescriptor': {'Location': 's3://some-location/2024'},
-            'Parameters': {}
-        }
+    mock_partition = {
+        'Values': ['2024'],
+        'StorageDescriptor': {'Location': 's3://some-location/2024'},
+        'Parameters': {}
+    }
 
-        glue = mock.MagicMock()
-        glue.batch_create_partition = mock.MagicMock()
+    glue = mock.MagicMock()
 
-        # Mock paginators for source and target
+    # Patch batch_create_partition with the correct binding
+    with patch.object(glue, 'batch_create_partition') as batch_mock:
         source_paginator = mock.MagicMock()
         target_paginator = mock.MagicMock()
 
         source_paginator.paginate.return_value = [{'Partitions': [mock_partition]}]
         target_paginator.paginate.return_value = [{'Partitions': []}]
 
-        def get_paginator(op):
-            paginator = mock.MagicMock()
-            paginator.paginate.side_effect = lambda **kwargs: source_paginator.paginate() if kwargs['DatabaseName'] == source_db else target_paginator.paginate()
-            return paginator
+        def get_paginator(name):
+            return source_paginator if name == 'get_partitions' else target_paginator
 
-        glue.get_paginator.side_effect = get_paginator
+        glue.get_paginator.side_effect = lambda op: source_paginator if op == 'get_partitions' else target_paginator
 
         common_data_sync.sync_table_partitions(source_db, target_db, table_name, glue)
 
-        self.assertTrue(glue.batch_create_partition.called)
-
-if __name__ == "__main__":
-    unittest.main()
+        batch_mock.assert_called_once()
