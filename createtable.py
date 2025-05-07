ef021307273361010ce94d55dbd8b095561df03b
JaichandranR <highmast@gmail.com>
2025-05-07 14:48:52 -0400

    Update createtable.py

--- createtable.py
+++ createtable.py
@@ -1,24 +1,28 @@
-        glue = mock.MagicMock()
+def test_sync_table_partitions(self):
+    source_db = 'src'
+    target_db = 'tgt'
+    table_name = 'test_table'
 
-        mock_partition = {
-            'Values': ['2024'],
-            'StorageDescriptor': {'Location': 's3://some-location/2024'},
-            'Parameters': {}
-        }
+    mock_partition = {
+        'Values': ['2024'],
+        'StorageDescriptor': {'Location': 's3://some-location/2024'},
+        'Parameters': {}
+    }
 
-        glue.batch_create_partition = mock.MagicMock()
+    glue = mock.MagicMock()
 
-        def get_paginator_side_effect(op):
-            paginator = mock.MagicMock()
-            if op == 'get_partitions':
-                paginator.paginate.side_effect = lambda **kwargs: (
-                    [{'Partitions': [mock_partition]}] if kwargs['DatabaseName'] == source_db
-                    else [{'Partitions': []}]
-                )
-            return paginator
+    def get_paginator_side_effect(op):
+        paginator = mock.MagicMock()
+        if op == 'get_partitions':
+            paginator.paginate.side_effect = lambda **kwargs: (
+                [{'Partitions': [mock_partition]}] if kwargs['DatabaseName'] == source_db
+                else [{'Partitions': []}]
+            )
+        return paginator
 
-        glue.get_paginator.side_effect = get_paginator_side_effect
+    glue.get_paginator.side_effect = get_paginator_side_effect
+    glue.batch_create_partition = mock.MagicMock()
 
-        common_data_sync.sync_table_partitions(source_db, target_db, table_name, glue)
+    common_data_sync.sync_table_partitions(source_db, target_db, table_name, glue)
 
-        glue.batch_create_partition.assert_called_once()
+    glue.batch_create_partition.assert_called_once()
