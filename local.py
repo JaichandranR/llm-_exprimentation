c6e801316c36fce07f84d52cfd07e41aab17bbf5
JaichandranR <highmast@gmail.com>
2025-06-11 12:47:47 -0400

    Update local.py

--- local.py
+++ local.py
@@ -1,13 +1,8 @@
 SELECT 
-  c.table_schema,
-  c.table_name,
-  c.column_name,
-  c.data_type
-FROM apitables.information_schema.columns c
-JOIN apitables.information_schema.tables t
-  ON c.table_schema = t.table_schema
-  AND c.table_name = t.table_name
-WHERE c.table_schema != 'information_schema'
-  AND t.table_type = 'BASE TABLE'
-ORDER BY c.table_schema, c.table_name, c.ordinal_position
-LIMIT 1000;
+  table_schema,
+  table_name,
+  column_name,
+  data_type
+FROM cosmos_nonhcd_iceberg.information_schema.columns
+WHERE table_schema != 'information_schema'
+ORDER BY table_schema, table_name, ordinal_position;
