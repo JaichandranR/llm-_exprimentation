145e93ae666787e07db996a85764f7c62faa2c76
JaichandranR <highmast@gmail.com>
2025-06-12 16:09:34 -0400

    Update local.py

--- local.py
+++ local.py
@@ -1,8 +1,8 @@
-- id: refined_archer_control_objective_status
+- id: check_status
   type: io.kestra.plugin.core.log.Log
   message: >
     {% set out = outputs.refined_archer_control_objective.apiResponse.output %}
-    {% if out.contains("SUCCESS") %}
+    {% if out.matches("(?s).*SUCCESS.*") %}
       Status = Success
     {% else %}
       Status = Failure
