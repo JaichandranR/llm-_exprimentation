14b152fd816a5c1457b90570479165d84253793c
JaichandranR <highmast@gmail.com>
2025-10-13 01:57:39 -0400

    Update table.sql

--- table.sql
+++ table.sql
@@ -1,9 +1,22 @@
 {{
     config(
         materialized = 'table',
-        on_table_exists = 'append'
+        on_table_exists = 'replace'
     )
 }}
 
-{# /* Run macro directly to create + insert RCC audit results */ #}
+{# /* Run the macro to auto-create + populate audit table */ #}
 {% do audit_rcc_codes(50) %}
+
+{# /* Return a valid query so dbt can compile the model */ #}
+select
+    model_name,
+    schema_name,
+    database_name,
+    rcc_code,
+    purge_date_field,
+    retention_value,
+    status,
+    message,
+    scan_timestamp
+from {{ this }}
